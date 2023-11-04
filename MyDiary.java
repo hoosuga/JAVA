3aaf6d31087413dd2993387e67f58303e4170581
hoosuga <130055556+hoosuga@users.noreply.github.com>
2023-11-04 09:29:06 +0000

    [Add] MyDiary

---
+++ MyDiary.java
@@ -0,0 +1,10 @@
+public class MyDiary {
+  public static void main(String[] args) {
+    int version;
+    version = 1 + 5;
+    System.out.println("日記ソフト");
+    System.out.println("ver" + version);
+    System.out.println("まだ開発中です");
+    System.out.println("終了します");
+  }
+}

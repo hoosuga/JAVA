1e24ec7dd49d5f5d9390f92d0c11930decbf1225
hoosuga <130055556+hoosuga@users.noreply.github.com>
2023-11-04 10:22:01 +0000

    [Add] 練習1-3

--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
 public class Main {
-  public static void main(String[] args) {
-    int a = 3;
-    int b = 5;
-    int c = a * b;
-    System.out.println("縦幅"+ a + "横幅" + b + "の長方形の面積は、" + c);
+  public static void main(String[] ages) {
+    boolean result = true;
+    char name = '駆';
+    double pi = 3.14;
+    long number = 314159265853979L;
+    String msg = "ミナトの攻撃！敵に15ポイントのダメージを与えた。" ;
   }
 }

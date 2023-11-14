f7c887396ea3b61ab9de54510d80a76b5126ae3c
hoosuga <130055556+hoosuga@users.noreply.github.com>
2023-11-14 10:37:38 +0000

    [Add] 波かっこを省略した記述

--- Main.java
+++ Main.java
@@ -1,9 +1,10 @@
 public class Main {
   public static void main(String[] args) {
-    boolean doorClose = true;
-    while (doorClose == true) {
-      System.out.println("ノックする");
-      System.out.println("1分待つ");
-    }
+    boolean tenki = false;
+    if (tenki == true) {
+      System.out.println("洗濯をします");
+      System.out.println("散歩に行きます");
+    } else
+      System.out.println("DVDを見ます");
   }
 }

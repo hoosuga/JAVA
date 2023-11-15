6fbb5c3a1e7dcb372952530fa359f97bc98b0949
hoosuga <130055556+hoosuga@users.noreply.github.com>
2023-11-15 10:04:07 +0000

    [Add] 冗長でスッキリしないソースコード

--- Main.java
+++ Main.java
@@ -1,10 +1,15 @@
 public class Main {
   public static void main(String[] args) {
-    boolean tenki = false;
-    if (tenki == true) {
-      System.out.println("洗濯をします");
-      System.out.println("散歩に行きます");
-    } else
-      System.out.println("DVDを見ます");
+    System.out.println("あなたの運勢を占います");
+    int fortune = new java.util.Random().nextInt(4) + 1;
+    if (fortune == 1) {
+      System.out.println("大吉");
+    } else if (fortune == 2) {
+      System.out.println("中吉");
+    } else if (fortune == 3) {
+      System.out.println("吉");
+    } else {
+      System.out.println("凶");
+    }
   }
 }
